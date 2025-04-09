fe7c541dbdc2fea60b18e92347327e7f9319dcff
Gatanot <114661687+Gatanot@users.noreply.github.com>
2025-04-09 10:44:48 +0800

    Update com_GeneticAlgTSP.py

--- lab3/com_GeneticAlgTSP.py
+++ lab3/com_GeneticAlgTSP.py
@@ -1,27 +1,43 @@
 from GeneticAlgTSP import GeneticAlgTSP
+from time import strftime
+from time import localtime
 import matplotlib.pyplot as plt
 
 
-def test_iteration():
-    iterations = [i for i in range(100, 1600, 100)]
+test_files = ["wi29.tsp", "dj38.tsp", "qa194.tsp"]
+result_file = address = strftime("%Y_%m_%d_%H_%M", localtime()) + ".md"
+
+
+def save(testname, var, distance):
+
+    with open(result_file, "a") as f:
+        f.write(f"# {testname}\n")
+        f.write(f"|var|distance|\n")
+        f.write(f"|---|---|\n")
+        for i in range(len(var)):
+            f.write(f"|{var[i]}|{distance[i]}|\n")
+
+
+def test_iteration(filename):
+    iterations = [i for i in range(100, 2550, 50)]
     print(iterations)
     best_distances = []
     for i in iterations:
-        ga = GeneticAlgTSP("dj38.tsp")
+        ga = GeneticAlgTSP(filename)
         ga.iterate(i)
         best_distances.append(ga.best_distance)
-    plot_connected_points(iterations, best_distances)
+    return iterations, best_distances
 
 
-def test_mutation_rate():
+def test_mutation_rate(filename):
     mutation_rates = [i for i in range(1, 11, 1)]
     print(mutation_rates)
     best_distances = []
     for i in mutation_rates:
-        ga = GeneticAlgTSP("dj38.tsp", mutation_rate=i / 10)
+        ga = GeneticAlgTSP(filename, mutation_rate=i / 10)
         ga.iterate(100)
         best_distances.append(ga.best_distance)
-    plot_connected_points(mutation_rates, best_distances)
+    return mutation_rates, best_distances
 
 
 def plot_connected_points(x_coords, y_coords):
@@ -44,5 +60,10 @@ def plot_connected_points(x_coords, y_coords):
     plt.show()
 
 
-test_iteration()
-test_mutation_rate()
+def test_save_iteration():
+    for file in test_files:
+        vars, distance = test_iteration("data\\" + file)
+        save("data\\" + file, vars, distance)
+
+
+test_save_iteration()
